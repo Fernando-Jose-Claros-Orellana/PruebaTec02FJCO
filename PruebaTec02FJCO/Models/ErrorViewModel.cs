9f26a29468217f4c5820b5dd301dcae190a17302
Fernando FJ <ferinocente357@gmail.com>
2024-02-21 09:46:03 -0600

    Agregar archivos de proyecto.

---
+++ PruebaTec02FJCO/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PruebaTec02FJCO.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
